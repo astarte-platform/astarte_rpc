24d1984079bb10104e38e81be511e90cdce26f04
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-28 11:09:22 +0200

    Initial commit

---
+++ lib/astarte_rpc.ex
@@ -0,0 +1,18 @@
+defmodule AstarteRpc do
+  @moduledoc """
+  Documentation for AstarteRpc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AstarteRpc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/astarte_rpc_test.exs
@@ -0,0 +1,8 @@
+defmodule AstarteRpcTest do
+  use ExUnit.Case
+  doctest AstarteRpc
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
