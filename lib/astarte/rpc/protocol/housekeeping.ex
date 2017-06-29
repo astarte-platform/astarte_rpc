4a2b186b67494b28643c1c4ce37fce5be73d51b6
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-29 13:12:56 +0200

    Move protobufs inside lib tree
    Avoid .. madness

--- lib/astarte/rpc/protocol/housekeeping.ex
+++ lib/astarte/rpc/protocol/housekeeping.ex
@@ -1,5 +1,5 @@
 defmodule Astarte.RPC.Protocol.Housekeeping do
-  @external_resource Path.expand("../../proto/housekeeping", __DIR__)
+  @external_resource Path.expand("proto/housekeeping", __DIR__)
 
-  use Protobuf, from: Path.wildcard(Path.expand("../../proto/housekeeping/*.proto", __DIR__))
+  use Protobuf, from: Path.wildcard(Path.expand("proto/housekeeping/*.proto", __DIR__))
 end
--- lib/astarte/rpc/protocol/realm_management.ex
+++ lib/astarte/rpc/protocol/realm_management.ex
@@ -1,5 +1,5 @@
 defmodule Astarte.RPC.Protocol.RealmManagement do
-  @external_resource Path.expand("../../proto/realm_management", __DIR__)
+  @external_resource Path.expand("proto/realm_management", __DIR__)
 
-  use Protobuf, from: Path.wildcard(Path.expand("../../proto/realm_management/*.proto", __DIR__))
+  use Protobuf, from: Path.wildcard(Path.expand("proto/realm_management/*.proto", __DIR__))
 end
