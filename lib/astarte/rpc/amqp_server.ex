340392df4ba3db5c563224af5fd59d3bcb0e770b
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-06-30 15:23:51 +0200

    amqp_server: remove dependencies from external configuration
    First step in moving to __using__ macro

--- lib/astarte/rpc/amqp_server.ex
+++ lib/astarte/rpc/amqp_server.ex
@@ -6,85 +6,78 @@ defmodule Astarte.RPC.AMQPServer do
   use AMQP
 
   @connection_backoff 10000
+  @queue "rpc_queue"
 
-  def start_link(amqp_opts, consumer_opts, name \\ nil) do
-    GenServer.start_link(__MODULE__, {amqp_opts, consumer_opts}, name: name)
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
   end
 
-  def init({amqp_opts, consumer_opts}) do
-    consumer_queue = Keyword.get(consumer_opts, :queue)
-    consumer_callback = Keyword.get(consumer_opts, :callback)
-    state = %{amqp_opts: amqp_opts,
-              consumer_queue: consumer_queue,
-              consumer_callback: consumer_callback,
-              chan: nil
-            }
-    rabbitmq_connect(state, false)
+  def init(_opts) do
+    rabbitmq_connect(false)
   end
 
-  defp rabbitmq_connect(state, retry \\ true) do
-    with {:ok, options} <- Map.fetch(state, :amqp_opts),
-         {:ok, conn} <- Connection.open(options),
+  defp rabbitmq_connect(retry \\ true) do
+    options = []
+    with {:ok, conn} <- Connection.open(options),
          # Get notifications when the connection goes down
          Process.monitor(conn.pid),
          # We link the connection to this process, that way if we die the connection dies too
          # This is useful since unacked messages are requeued only after the connection is dead
          Process.link(conn.pid),
          {:ok, chan} <- Channel.open(conn),
-         {:ok, queue} <- Map.fetch(state, :consumer_queue),
-         {:ok, _consumer_tag} <- Basic.consume(chan, queue) do
-      {:ok, %{state | chan: chan}}
+         {:ok, _consumer_tag} <- Basic.consume(chan, @queue) do
+      {:ok, chan}
 
     else
       {:error, reason} ->
         Logger.warn("RabbitMQ Connection error: " <> inspect(reason))
-        maybe_retry(state, retry)
+        maybe_retry(retry)
       :error ->
         Logger.warn("Unknown RabbitMQ connection error")
-        maybe_retry(state, retry)
+        maybe_retry(retry)
     end
   end
 
-  defp maybe_retry(state, retry) do
+  defp maybe_retry(retry) do
     if retry do
       :timer.sleep(@connection_backoff)
-      rabbitmq_connect(state)
+      rabbitmq_connect(retry)
     else
-      {:ok, state}
+      {:ok, nil}
     end
   end
 
   # Server callbacks
 
   # Confirmation sent by the broker after registering this process as a consumer
-  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, state) do
-    {:noreply, state}
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, chan) do
+    {:noreply, chan}
   end
 
   # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
-  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, state) do
-    {:stop, :normal, state}
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, chan) do
+    {:stop, :normal, chan}
   end
 
   # Confirmation sent by the broker to the consumer process after a Basic.cancel
-  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, state) do
-    {:noreply, state}
+  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, chan) do
+    {:noreply, chan}
   end
 
-  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, state) do
+  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, chan) do
     # We process the message asynchronously
-    spawn_link fn -> consume(Map.get(state, :chan), tag, redelivered, payload, Map.get(state, :consumer_callback)) end
-    {:noreply, state}
+    spawn_link fn -> consume(chan, tag, redelivered, payload) end
+    {:noreply, chan}
   end
 
   # This callback should try to reconnect to the server
-  def handle_info({:DOWN, _, :process, _pid, _reason}, state) do
-    {:ok, state} = rabbitmq_connect(state)
-    {:noreply, state}
+  def handle_info({:DOWN, _, :process, _pid, _reason}, _chan) do
+    {:ok, new_chan} = rabbitmq_connect()
+    {:noreply, new_chan}
   end
 
-  defp consume(chan, tag, redelivered, payload, callback) do
-    case callback.(payload) do
+  defp consume(chan, tag, redelivered, payload) do
+    case IO.puts(inspect(payload)) do
       :ok -> Basic.ack(chan, tag)
       # We don't want to keep failing on the same message
       {:error, reason} ->
