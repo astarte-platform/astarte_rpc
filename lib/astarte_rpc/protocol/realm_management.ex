0e6db45836074a22d63d96979ca454d1c044b9da
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-07-12 17:11:04 +0200

    realm_management: use Protocol

--- lib/astarte_rpc/protocol/realm_management.ex
+++ lib/astarte_rpc/protocol/realm_management.ex
@@ -2,4 +2,5 @@ defmodule Astarte.RPC.Protocol.RealmManagement do
   @external_resource Path.expand("proto/realm_management", __DIR__)
 
   use Protobuf, from: Path.wildcard(Path.expand("proto/realm_management/*.proto", __DIR__))
+  use Astarte.RPC.Protocol
 end
