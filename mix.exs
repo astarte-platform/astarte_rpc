d365b2d8f6028ccbec6dead608c687d1d2080c47
Davide Bettio <davide.bettio@ispirata.com>
2018-10-22 13:08:49 +0200

    Prepare next development cycle
    Next release will be 0.10.0-beta.3.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Astarte.RPC.Mixfile do
   def project do
     [
       app: :astarte_rpc,
-      version: "0.10.0-beta.2",
+      version: "0.10.0-beta.3",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
