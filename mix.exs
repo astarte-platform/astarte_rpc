3a3027002a0e0403261c4e7403c2c71b9f3a6233
Davide Bettio <davide.bettio@ispirata.com>
2019-01-29 12:38:17 +0100

    Astarte once released will have a different license
    Change license to Apache 2.0 before release candidate as described
    on the roadmap.

--- mix.exs
+++ mix.exs
@@ -1,3 +1,21 @@
+#
+# This file is part of Astarte.
+#
+# Copyright 2017 Ispirata Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 defmodule Astarte.RPC.Mixfile do
   use Mix.Project
 
@@ -60,7 +78,7 @@ defmodule Astarte.RPC.Mixfile do
   defp package do
     [
       maintainers: ["Davide Bettio", "Riccardo Binetti"],
-      licenses: ["LGPL-3.0-or-later"],
+      licenses: ["Apache-2.0"],
       links: %{
         "Astarte" => "https://astarte-platform.org",
         "Ispirata" => "https://ispirata.com",
