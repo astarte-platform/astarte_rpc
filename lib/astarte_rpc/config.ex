d72a09f17b7f7786d4fbd138f260d829b1c57d95
Mattia Mazzucato <matt.mazzucato@gmail.com>
2020-05-21 11:29:09 +0200

    Default max certificate chain length to 10
    The erlang default of 1 is too strict. Increase the default value to 10
    to make it coincident to OpenSSL default.
    
    Signed-off-by: Mattia Mazzucato <matt.mazzucato@gmail.com>

--- lib/astarte_rpc/config.ex
+++ lib/astarte_rpc/config.ex
@@ -114,6 +114,7 @@ defmodule Astarte.RPC.Config do
           {:cacertfile, String.t()}
           | {:verify, :verify_peer}
           | {:server_name_indication, :disable | charlist()}
+          | {:depth, integer()}
   @type ssl_options :: :none | [ssl_option]
 
   @type options ::
@@ -154,7 +155,8 @@ defmodule Astarte.RPC.Config do
   defp build_ssl_options() do
     [
       cacertfile: amqp_connection_ssl_ca_file!() || CAStore.file_path(),
-      verify: :verify_peer
+      verify: :verify_peer,
+      depth: 10
     ]
     |> populate_sni()
   end
