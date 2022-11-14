209648e99bd85eaff6772dfc57f2aa398b27e5ae
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2022-11-14 16:19:55 +0100

    Bump Elixir to 1.14.0-otp-25 and Erlang to 25.1.1
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Astarte.RPC.Mixfile do
     [
       app: :astarte_rpc,
       version: "1.1.0-dev",
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
