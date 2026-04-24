22e09a03fb18c1326ee5ebf1ae6a7abef0109d85
frossq <francesco.asquini@secomind.com>
2026-04-24 15:44:34 +0200

    chore: prepare v1.2.2
    Signed-off-by: frossq <francesco.asquini@secomind.com>

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Astarte.RPC.Mixfile do
   def project do
     [
       app: :astarte_rpc,
-      version: "1.2.2-rc.0",
+      version: "1.2.2",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
